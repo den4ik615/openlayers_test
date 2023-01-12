a02a8ca3f66e48bf6a1989a249c27d2ec44b1b48
Den4ik615 <Den4ik615@rambler.ru>
2023-01-12 22:50:17 +0300

    с авто отрисовкой прямоугольников и полигонов

--- src/map/layers/vector/vector.tsx
+++ src/map/layers/vector/vector.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { MapBrowserEvent } from "ol";
 import { Interaction } from "ol/interaction";
+import Draw, {
+  createBox,
+  createRegularPolygon,
+} from "ol/interaction/Draw";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import {getArea} from 'ol/sphere'
@@ -20,6 +24,7 @@ import { Coordinate } from "ol/coordinate";
 import LinearRing from "ol/geom/LinearRing";
 import { features } from "process";
 import Geometry from "ol/geom/Geometry";
+import GeometryType from "ol/geom/GeometryType";
 
 const selectStyle = new Style({
   fill: new Fill({
@@ -45,9 +50,12 @@ let elm : Feature;
 elm=new Feature({
 });
 
+let draw : Draw; // global so we can remove it later
+
 export const SelecElm = {
-  elem : elm,
-  flag : false
+  elem : elm, //выделенный объект
+  flag : false, //был загружен объект из файла
+  type : 'None' //тип отрисовываемой фигуры
 };
 
 class VectorLayerComponent extends React.PureComponent<
@@ -68,13 +76,13 @@ class VectorLayerComponent extends React.PureComponent<
       source: this.source,
     });
 
-    this.cnt = 0;
-    this.points = [];
+    this.cnt = 0; // количество точек 
+    this.points = []; //массви точек вручную рисуемого объекта
 
-    this.props.map.addLayer(this.layer);
-    this.props.map.on("singleclick", this.onMapClick);
-    this.props.map.on("dblclick",this.dblMapClick);
-    this.props.map.on("pointermove",this.selectObj);
+    this.props.map.addLayer(this.layer);//доюавляем слой на карту
+    this.props.map.on("singleclick", this.onMapClick);//обработка событий одиночных кликов
+    this.props.map.on("dblclick",this.dblMapClick);//двойной клик
+    this.props.map.on("pointermove",this.selectObj);//перемещение курсора, для выделения обЪекта
 
 
   }
@@ -95,70 +103,83 @@ class VectorLayerComponent extends React.PureComponent<
 
   selectObj = (event: MapBrowserEvent) =>
   {
-    let sel : FeatureLike[];
-    sel = this.props.map.getFeaturesAtPixel(event.pixel);
+    let sel : FeatureLike[];//массив выделенных объектов(полигонов)
+    sel = this.props.map.getFeaturesAtPixel(event.pixel);//объект(полигон) под курсором
     //console.log(sel.length)
 
-    if(SelecElm.flag)
+    if(SelecElm.flag)//был загружен полигон из файла
     {
-      this.source.addFeatures([SelecElm.elem]);
-      SelecElm.flag = false;
+      this.source.addFeatures([SelecElm.elem]);//добавляем полигон на карту
+      SelecElm.flag = false;//сбрасываем флаг загрузки из файла
     }
 
-    if(sel.length)
+    if(sel.length)//есть выделенные элементы
     {
-      this.selected = sel[0];
-      SelecElm.elem=this.selected;
-      //console.log(SelecElm.elem.getProperties().name)
-      //console.log(document.getElementById("input_name")?.getAttribute('value')?.toString())
-      this.selected.setStyle(selectStyle)
-      document.getElementById("select_name")?.setAttribute('value',this.selected.getProperties().name)
-      document.getElementById("input_square")?.setAttribute('value',this.selected.getProperties().square)
+      this.selected = sel[0];//текущий элемент
+      SelecElm.elem=this.selected;//текущий элемент для передачи на верхний уровень для обработки в меню
+      this.selected.setStyle(selectStyle)//задаем стиль выделенному полигону
+      this.selected.setProperties({'square':getArea(this.selected.getGeometry())/1000/1000});//высчитываем площадь
+      document.getElementById("select_name")?.setAttribute('value',this.selected.getProperties().name)//отображаем в инпуте имя текущего полигона
+      document.getElementById("input_square")?.setAttribute('value',this.selected.getProperties().square)//отображаем в инпуте площадь текущего полигона
     }
-    else
+    else//убран курсор с полигона
     {
-      if (this.selected !== null) {
-        console.log("clear style");
-        this.selected.setStyle(undefined);
-        this.selected = null;
+      if (this.selected !== null) //был выделен полигон
+      {
+        this.selected.setStyle(undefined);//очищаем стиль
+        this.selected = null; //обнуляем выделенный полигон
       }
     }
-    this.props.map.forEachFeatureAtPixel(event.pixel, function (f) {
-      selectStyle.getFill().setColor(f.get('COLOR') || '#eeeeee');
-      return true;
-    })
   
   }
 
+  //обраюотка кликов по карте для ручного построения полигона
   onMapClick = (event: MapBrowserEvent) => {
     //console.log(document.getElementById("type")?.getAttribute('options'));
-    this.cnt ++;
-    const featureToAdd = new Feature({
-      geometry: new Point(event.coordinate),
-      name: "Point",
-    });
-    //const point = new Point(event.coordinate);
-    this.points.push(event.coordinate);
-    const style = new Style({
-      image: new Circle({
-        radius: 3,
-        fill: new Fill({color: 'red'}),
-        stroke: new Stroke({
-          color: [0,0,0], width: 2
+    if(SelecElm.type==="None")//ручное построение
+    {
+      this.props.map.removeInteraction(draw);//удаляем текущее рисование
+      this.cnt ++;//насчитываем количество точек
+      const featureToAdd = new Feature({ //новая точка
+        geometry: new Point(event.coordinate), //с текущими координатами
+        name: "Point",
+      });
+      //const point = new Point(event.coordinate);
+      this.points.push(event.coordinate);//накапливаем массив координат
+      const style = new Style({//стиль точки 
+        image: new Circle({
+          radius: 3,
+          fill: new Fill({color: 'red'}),
+          stroke: new Stroke({
+            color: [0,0,0], width: 2
+          })
         })
-      })
-    });
-    // var line_feat1 = new Feature({
-    //   geometry: new LineString(this.source.getFeatures,featureToAdd.getGeometry),
-    //   name: "My_Simple_LineString"
-    // });
-    featureToAdd.setStyle(style);    
-    //var vector = new this.props.map.;
-    //vector.addFeatures([new OpenLayers.Feature.Vector(new OpenLayers.Geometry.LineString([start_point, end_point]))]);
-    //map.addLayers([vector]);
-    //this.source.clear();
-    //this.source.
-    this.source.addFeatures([featureToAdd]);
+      });
+      featureToAdd.setStyle(style); //задаем стиль
+      this.source.addFeatures([featureToAdd]);//добавляем на слой точку
+    }
+    else//анализируем выбранный тип объекта
+    {
+        const value = SelecElm.type;
+        if (value === 'Polygon') { //полигон
+          this.props.map.removeInteraction(draw);//удаляем предыдущий тип рисования
+          draw = new Draw({//создаем тип отрисовки
+            source: this.source,
+            type: GeometryType.POLYGON
+          });
+          this.props.map.addInteraction(draw);//передаем на обработку
+        }
+        if (value === 'Box') {//прямоугольник
+          this.props.map.removeInteraction(draw);//удаляем предыдущий тип рисования
+          draw = new Draw({
+            source: this.source,
+            type: GeometryType.CIRCLE,
+            geometryFunction: createBox()//функция отрисовки прямоугольника
+          });
+          this.props.map.addInteraction(draw);//передаем на обработку
+        }
+        SelecElm.type='brr';//сбрасываем тип чтоб не возникло повторной обработки отрисовки
+    }
     //console.log(this.cnt);
   };
 
@@ -166,35 +187,32 @@ class VectorLayerComponent extends React.PureComponent<
     if (this.cnt>3)
     {
       let i = 0; 
-      this.points.push(this.points[0]);
-      //let linearRing = new LinearRing(this.points);
-      //console.log(linearRing.getCoordinates())
-      //let sitePoints = [];
+      this.points.push(this.points[0]);//замыкаем полигон добявляя в конец первую точку
       this.source.forEachFeature(element => {
-        //if(element.getGeometryName()==)
-        //this.points[i].transform("EPSG:4326",)
-        if(element.getGeometry().getType().toString()==="Point")
+        //удаляем все точки
+        if(element.getGeometry().getType().toString()==="Point")//если элемент точка
         {
-          this.source.removeFeature(element);
+          this.source.removeFeature(element);//удаляем
         }
         else
         {
           console.log(i++)
         }
       });
+      //создаем новый полигон
        const featurePolygon = new Feature({
          //geometry: new Polygon([linearRing.getCoordinates()]),
-         geometry: new Polygon([this.points]),
-         name: "test",
-         square: 0,
+         geometry: new Polygon([this.points]),//полигон из накопленных координат
+         name: "test",//имя по умолчанию
+         square: 0,//площадь
        });
-       featurePolygon.setProperties({'square':getArea(featurePolygon.getGeometry())/1000/1000});
+       featurePolygon.setProperties({'square':getArea(featurePolygon.getGeometry())/1000/1000});//вычисляем площадь
        //featurePolygon.setProperties('square':featurePolygon.getGeometry());
        //this.source.clear();
-       this.source.addFeatures([featurePolygon]);
-       console.log(featurePolygon.getProperties().square);
-       console.log(featurePolygon.getProperties().name)
-       this.points.length=0;
+       this.source.addFeatures([featurePolygon]);//добавляем полигон
+       //console.log(featurePolygon.getProperties().square);
+       //console.log(featurePolygon.getProperties().name)
+       this.points.length=0; //обнуляем массив координат
        this.cnt=0;
     }
   };
@@ -208,6 +226,7 @@ class VectorLayerComponent extends React.PureComponent<
 export const VectorLayerWithContext = (props: TVectorLayerProps) => {
   console.log(props);
   return (
+    //возвращаем слой с содержимым
     <MapContext.Consumer>
       {(mapContext: IMapContext | void) => {
         if (mapContext) {
--- src/map/map.tsx
+++ src/map/map.tsx
@@ -30,83 +30,82 @@ export class MapComponent extends React.PureComponent<TMapProps, TMapState> {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
-    this.renamePolygon = this.renamePolygon.bind(this);
-    this.savePolygon = this.savePolygon.bind(this);
-    this.openPolygon = this.openPolygon.bind(this);
+    this.renamePolygon = this.renamePolygon.bind(this);//переименование полигона
+    this.savePolygon = this.savePolygon.bind(this);//сохранить в файл
+    this.openPolygon = this.openPolygon.bind(this);//открыть из файла
+    this.selectType = this.selectType.bind(this);//выбор типа отрисовки
     this.mapDivRef = React.createRef<HTMLDivElement>();
     //this.fileInput = React.createRef();
     //this.popup = React.createRef<HTMLDivElement>();
     //this.overlay = React.createRef<HTMLDivElement>();
   }
 
-  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    //document.getElementById("input").setAttribute('value','e.target.value.toString()')
-    //e.target.setAttribute('value',e.target.value.toString());
-    document.getElementById("input_name")?.setAttribute('value',e.target.value.toString())
-    //e.target.value="1234567";
-    console.log("handleChange");
-    console.log(e.target.value.toString());
+  selectType(e: React.ChangeEvent<HTMLSelectElement>)//обрабатываем выбранный тип
+  {
+      SelecElm.type=e.target.value;//передаем на другой уровень
   }
-
+  //новое имя файла
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    document.getElementById("input_name")?.setAttribute('value',e.target.value.toString())
+    //console.log("handleChange");
+    //console.log(e.target.value.toString());
+  }
+  //переименование файла
   renamePolygon(e: React.FormEvent<HTMLFormElement>) {
-      alert('Отправленное имя: ' + document.getElementById("input_name")?.getAttribute('value')?.toString() );
-      e.preventDefault();
-      //let elm : Feature;
-      //elm = SelecElm.elem;
-      SelecElm.elem.setProperties({'name':document.getElementById("input_name")?.getAttribute('value')?.toString()});
-      console.log(SelecElm.elem.getProperties().name);
+      alert('Отправленное имя: ' + document.getElementById("input_name")?.getAttribute('value')?.toString() );//уведомление о переименовании
+      e.preventDefault();//отменяем перезагрузку страницы по умолчанию
+      SelecElm.elem.setProperties({'name':document.getElementById("input_name")?.getAttribute('value')?.toString()});//задаем новое имя
+      //console.log(SelecElm.elem.getProperties().name);
   }
 
   savePolygon(e: React.FormEvent<HTMLFormElement>)
   {
     alert('Сохранить полигон: ' + SelecElm.elem.getProperties().name );
     e.preventDefault();
-    //const elements: Feature[] = [];
-    //elements.push(SelecElm.elem);
+    //создаем новый геоджейсон объект с информацией о выделенном полигоне
     const json = new GeoJSON().writeFeatureObject(SelecElm.elem, { 
 
       dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'
 
     });
 
-    console.log(SelecElm.elem);
+    //console.log(SelecElm.elem);
     const contentType='application/json';
-
+    //создаем элемент ссылки для сохранения файла
     var a = document.createElement("a");
-
+    //новый файл с содержимым из геоджейсон
     var file = new Blob([JSON.stringify(json)], {type: contentType});
-
+    //добавляем файл
     a.href = URL.createObjectURL(file);
 
     a.download = SelecElm.elem.getProperties().name + '.json';
-
+    //скачиваем имитируя нажатие на ссылку
     a.click();
   }
-
+  //загрузка полигона из геоджейсон файла
   openPolygon(e: React.ChangeEvent<HTMLInputElement>)
   {
     //const json = new GeoJSON().readFeature(e.target.value.toString())
     //console.log(json)
     e.preventDefault();
-    const files = e.target.files;
-    if(files?.length)
+    const files = e.target.files;//информация о выбранном файле
+    if(files?.length)//файл выбран
     {
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        var content = reader.result;
+      var reader = new FileReader();//создаем ридер для чтения содержимого файла
+      reader.onload = function(e) {//файл загружен
+        var content = reader.result;//содержимое файла
         //JSON.parse(content)
-        if(content)
+        if(content)//файл не пустой
         {
           //var pars = JSON.parse(content.toString());
-          const json = new GeoJSON().readFeature(JSON.parse(content.toString()));
-          console.log(json);
-          SelecElm.elem = json;
-          SelecElm.flag = true;
+          const json = new GeoJSON().readFeature(JSON.parse(content.toString()));//преобразуем содержимое обратно в объект слоя
+          //console.log(json);
+          SelecElm.elem = json; //загруженный файл для отрисовки на слое
+          SelecElm.flag = true; //флаг загрузки файла
         }
-        //Here the content has been read successfuly
       }
 
-      reader.readAsText(files[0]);  
+      reader.readAsText(files[0]);  //вычитываем файл
     }
   }
 
@@ -114,7 +113,7 @@ export class MapComponent extends React.PureComponent<TMapProps, TMapState> {
     if (!this.mapDivRef.current) {
       return;
     }
-
+    //создаем карту
     const map = new Map({
       target: this.mapDivRef.current,
       layers: [
@@ -139,41 +138,17 @@ export class MapComponent extends React.PureComponent<TMapProps, TMapState> {
        console.log(map.getEventCoordinate(e));
      }, true)
 
-
-     // Basic overlay
-    // const overlay = new Overlay({
-    //   //position: [0,0],
-    //   element: this.overlay.current,
-    //   //positioning: 'center-center',
-    //   stopEvent: false
-    // });
-    // map.addOverlay(overlay);
-
-    // // Popup showing the position the user clicked
-    // this.popup = new Overlay({
-    //   element: ReactDOM.findDOMNode(this).querySelector('#popup')
-    // });
-
-    // // Listener to add Popup overlay showing the position the user clicked
-    // this.map.on('click', evt => {
-    //   this.popup.setPosition(evt.coordinate);
-    //   this.map.addOverlay(this.popup);
-    // })
-    // map.getViewport().addEventListener('click', (e) => {
-    //   console.log(e);
-    // }, true)
-
   }
 
-
+  //отрисовка страницы с содержимым
   render() {
     return (
       <div style={{ display:'flex'}}>
         <div data-options="region:'north'" ref={this.sourceDivRef} style={{color:"white",background:'#2D3E50',float:'right', width:210}} >
         <form>
-          <label>Geometry type</label>
-            <select id="type">
-              <option value="None">None</option>
+          <label>Тип фигуры : </label>
+            <select id="type" onChange={this.selectType}>
+              <option value="None">Manual</option>
               {/* <option value="Point">Point</option>
               <option value="LineString">LineString</option> */}
               <option value="Polygon">Polygon</option>
